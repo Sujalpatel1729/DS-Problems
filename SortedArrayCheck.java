de0890ec2b87c19de7d9bc38efab520dd52b804d
Sujalpatel1729 <sujal.24scse1420079@galgotiasuniversity.ac.in>
2025-09-21 13:23:46 +0530

    Add files via upload

---
+++ SortedArrayCheck.java
@@ -0,0 +1,18 @@
+class SortedArrayCheck{
+    public static boolean isSorted(int arr[], int index){
+        if(index == arr.length - 1){
+            return true;
+        }
+        if(arr[index] > arr[index + 1]){
+            return false;
+        }
+        else{
+            boolean remainingPart = isSorted(arr, index + 1);
+            return remainingPart;
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5};
+        System.out.println(isSorted(arr, 0));
+    }
+}
